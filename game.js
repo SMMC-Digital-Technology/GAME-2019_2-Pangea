886bc846e9ed63155ad72a1ac8834a1caa7d2059
Ben <alan.burt@mackillop.act.edu.au>
2019-11-13 12:06:29 +1100

    Added first sprite and resized the image. BS

--- game.js
+++ game.js
@@ -1,13 +1,14 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'Pangea', { preload: preload, create: create, update: update });
 
 function preload() {
-  game.load.image('astronaut' , 'astronaut.png')
+  game.load.spritesheet('astronaut' , 'assets/astronaut.png', 71, 84)
 
 }
 
 function create() {
 
   game.add.sprite(0, 0, 'astronaut');
+
 
 }
 
